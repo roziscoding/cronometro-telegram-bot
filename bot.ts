a03be7fda0e96f9eeb5b3f3d0391fe1524aab868
Rogerio Munhoz <roz@rjmunhoz.me>
2024-01-19 20:23:22 -0300

    feat: add blocking feature

--- bot.ts
+++ bot.ts
@@ -4,12 +4,50 @@ import { grammy, ms, z } from "./deps.ts";
 const kv = await Deno.openKv();
 const bot = new grammy.Bot(appConfig.TELEGRAM_TOKEN);
 
+bot.command("block", async (ctx) => {
+  const quotedMessage = ctx.message?.reply_to_message;
+
+  if (!quotedMessage) {
+    return ctx.reply("You need to reply to a message to use this command!");
+  }
+
+  if (!quotedMessage.from) {
+    return ctx.reply("You can only block users!");
+  }
+
+  await kv.set([quotedMessage.from.id.toString()], true);
+
+  await ctx.reply(`${quotedMessage.from.first_name} blocked!`);
+});
+
+bot.command("unblock", async (ctx) => {
+  const quotedMessage = ctx.message?.reply_to_message;
+
+  if (!quotedMessage) {
+    return ctx.reply("You need to reply to a message to use this command!");
+  }
+
+  if (!quotedMessage.from) {
+    return ctx.reply("You can only unblock users!");
+  }
+
+  await kv.delete([quotedMessage.from.id.toString()]);
+
+  await ctx.reply(`${quotedMessage.from.first_name} unblocked!`);
+});
+
 bot.command("remindme", async (ctx) => {
+  if (!ctx.from) return;
   if (!ctx.message?.reply_to_message) {
     return ctx.reply("You need to reply to a message to use this command!");
   }
 
   const quotedMessage = ctx.message.reply_to_message;
+
+  const isBlocked = await kv.get([ctx.from.id.toString()]).then(({ value }) => Boolean(value));
+
+  if (isBlocked) return;
+
   const time = ctx.message.text.split(" ").slice(1).join("");
   const msTime = ms(time);
 
