5c4eff97906f04572cca33151492eace32892210
Rogério Munhoz <roz@rjmunhoz.me>
2024-03-19 18:48:22 -0300

    feat: local message filter

--- src/bot.ts
+++ src/bot.ts
@@ -16,20 +16,23 @@ const openai = new OpenAI({ apiKey: appConfig.OPENAI_API_KEY });
 
 // bot.use(myCommands);
 
-bot.on("message:text", async (ctx) => {
-  // deno-lint-ignore no-explicit-any
-  ctx.api.config.use(addReplyParam(ctx as any) as any);
-  if (!ctx.message?.text) return;
-  const quotedMessage = ctx.message.reply_to_message;
+bot.on("message:text")
+  .filter((ctx) => ctx.hasChatType("private") || ctx.has(":entities:mention"))
+  .filter((ctx) => ctx.hasChatType("private") || ctx.entities("mention").join("") === `@${ctx.me.username}`)
+  .use(async (ctx) => {
+    // deno-lint-ignore no-explicit-any
+    ctx.api.config.use(addReplyParam(ctx as any) as any);
+    if (!ctx.message?.text) return;
+    const quotedMessage = ctx.message.reply_to_message;
 
-  await ctx.replyWithChatAction("typing");
+    await ctx.replyWithChatAction("typing");
 
-  await openai.beta.chat.completions.runTools({
-    model: "gpt-4",
-    messages: [
-      {
-        role: "system",
-        content: `
+    await openai.beta.chat.completions.runTools({
+      model: "gpt-4",
+      messages: [
+        {
+          role: "system",
+          content: `
           You are a helpful assistant that sets reminders, talking to the user via a telegram chatbot.
           Refuse to do anything that is not related to setting a reminder or obtaining the current date and time.
           You cannot set recurring reminders, nor can you set reminders for a date and time that has already passed.
@@ -42,82 +45,82 @@ bot.on("message:text", async (ctx) => {
           When calling setReminder, always provide the date and time in ISO 8601 format.
           Before taking any actions regarding setting a reminder, make sure to check for the current date and time.
         `,
-      },
-      { role: "user", content: ctx.message.text.replace("/gpt", "").trim() },
-    ],
-    tools: [
-      {
-        type: "function",
-        function: {
-          parse: JSON.parse,
-          function: async ({ date, message: text }: { date: string; message: string }) => {
-            const reminder = {
-              chatId: ctx.chat?.id,
-              messageId: quotedMessage?.message_id,
-              message: text,
-            };
+        },
+        { role: "user", content: ctx.message.text.replace("/gpt", "").trim() },
+      ],
+      tools: [
+        {
+          type: "function",
+          function: {
+            parse: JSON.parse,
+            function: async ({ date, message: text }: { date: string; message: string }) => {
+              const reminder = {
+                chatId: ctx.chat?.id,
+                messageId: quotedMessage?.message_id,
+                message: text,
+              };
 
-            const msTime = DateTime.fromISO(date).diffNow().toMillis();
+              const msTime = DateTime.fromISO(date).diffNow().toMillis();
 
-            if (msTime < 0) {
-              return { success: false, message: "Date and time must be in the future" };
-            }
+              if (msTime < 0) {
+                return { success: false, message: "Date and time must be in the future" };
+              }
 
-            await kv.enqueue(reminder, { delay: msTime });
-            return { succes: true, reminderTime: DateTime.now().plus(Duration.fromMillis(msTime)).toISO() };
-          },
-          name: "setReminder",
-          description: "Set a reminder for a specific date and time.",
-          parameters: {
-            type: "object",
-            properties: {
-              date: { type: "string", format: "date-time" },
-              message: { type: "string" },
+              await kv.enqueue(reminder, { delay: msTime });
+              return { succes: true, reminderTime: DateTime.now().plus(Duration.fromMillis(msTime)).toISO() };
             },
-            required: ["date", "message"],
+            name: "setReminder",
+            description: "Set a reminder for a specific date and time.",
+            parameters: {
+              type: "object",
+              properties: {
+                date: { type: "string", format: "date-time" },
+                message: { type: "string" },
+              },
+              required: ["date", "message"],
+            },
           },
         },
-      },
-      {
-        type: "function",
-        function: {
-          name: "getCurrentDate",
-          description: "Get the current date and time.",
-          function: () => {
-            return DateTime.now().toISO();
+        {
+          type: "function",
+          function: {
+            name: "getCurrentDate",
+            description: "Get the current date and time.",
+            function: () => {
+              return DateTime.now().toISO();
+            },
+            parameters: {},
           },
-          parameters: {},
         },
-      },
-    ],
-  })
-    .on("message", async (msg) => {
-      if (msg.role === "tool") {
-        console.log(`[${msg.role}] Tool ${calls.get(msg.tool_call_id)} returned ${msg.content}`);
-        calls.delete(msg.tool_call_id);
-      }
-      if (msg.role === "system") return;
-      if (msg.role === "user") return;
-      if (!msg.content && msg.role === "assistant" && msg.tool_calls) {
-        msg.tool_calls.forEach((call) => {
-          calls.set(call.id, call.function.name);
-          console.log(
-            `[${msg.role}] Calling ${call.function.name} with params ${call.function.arguments}`,
-          );
-        });
-        return;
-      }
-      if (msg.role === "assistant" && msg.content) await ctx.reply(msg.content);
-      console.log(`[${msg.role}] ${msg.content}`);
+      ],
     })
-    .on("error", (error) => {
-      console.error("Error", error);
-      ctx.reply(
-        `There was an error processing this message:\n<pre>${
-          error.message.split(".").map((l) => l.trim()).join(".\n")
-        }</pre>`,
-        { parse_mode: "HTML" },
-      );
-    })
-    .done();
-});
+      .on("message", async (msg) => {
+        if (msg.role === "tool") {
+          console.log(`[${msg.role}] Tool ${calls.get(msg.tool_call_id)} returned ${msg.content}`);
+          calls.delete(msg.tool_call_id);
+        }
+        if (msg.role === "system") return;
+        if (msg.role === "user") return;
+        if (!msg.content && msg.role === "assistant" && msg.tool_calls) {
+          msg.tool_calls.forEach((call) => {
+            calls.set(call.id, call.function.name);
+            console.log(
+              `[${msg.role}] Calling ${call.function.name} with params ${call.function.arguments}`,
+            );
+          });
+          return;
+        }
+        if (msg.role === "assistant" && msg.content) await ctx.reply(msg.content);
+        console.log(`[${msg.role}] ${msg.content}`);
+      })
+      .on("error", (error) => {
+        console.error("Error", error);
+        ctx.reply(
+          `There was an error processing this message:\n<pre>${
+            error.message.split(".").map((l) => l.trim()).join(".\n")
+          }</pre>`,
+          { parse_mode: "HTML" },
+        );
+      })
+      .done();
+  });
